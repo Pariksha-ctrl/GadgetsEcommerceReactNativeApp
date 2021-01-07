f9533371b08c7a0e659e89afb86d72562679d4c1
Pariksha Roka <parikshanepali@gmail.com>
2021-01-07 12:07:08 +1100

    Worked on hook called "useState" to save the data entered by the user. And also checked to see if it is working correctly by displaying it on the console.

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  // it is a hook called 'useState' from react to get and set the value
+  const [enteredGadget, setEnteredGadget] = useState("");
+
+  // function that will get the user input 'gadget' and set it
+  const gadgetInputHandler = (enteredText) => {
+    setEnteredGadget(enteredText);
+  };
+
+  // fuction that will handle the 'ADD' button to add the gadgets entered by the user
+  // it will add the entered gadget to the list of gadget
+  const addGadgetHandler = () => {
+    // to see that the entered gadgets are saved correctly
+    console.log(enteredGadget);
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Add Gadgets" style={styles.input} />
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Add Gadgets"
+          style={styles.input}
+          onChangeText={gadgetInputHandler}
+          value={enteredGadget}
+        />
+        <Button title="ADD" onPress={addGadgetHandler} />
       </View>
       <View />
     </View>
